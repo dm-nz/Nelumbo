0713d416ed306f77279bfdc332bc8e7af1437470
dmnz <nazzzim@gmail.com>
2018-11-17 17:23:23 +0600

    Load FontAwesome in header template

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -10,12 +10,9 @@
 
 function nelumbo_scripts() {
 	wp_register_style( 'foundation-style', get_template_directory_uri() . '/css/app.min.css', array(), false, 'all' );
-	wp_register_style( 'icons-style', 'https://use.fontawesome.com/releases/v5.2.0/css/all.css', array(), '5.2.0', 'all' );
 	wp_register_style( 'font-style', get_template_directory_uri() . '/fonts/lato/lato.css', array(), false, 'all' );
 
-	wp_enqueue_style( 'nelumbo-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'foundation-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'icons-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'font-style', get_stylesheet_uri() );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- header.php
+++ header.php
@@ -17,6 +17,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 </head>
 
 <body <?php body_class(); ?>>
