30a3fab73420f1920737715202c29074e1680a14
dmnz <nazzzim@gmail.com>
2018-12-24 14:20:33 +0600

    Return previous archive template

--- archive.php
+++ archive.php
@@ -1,49 +1,49 @@
 <?php
 /**
- * The template for displaying archive pages in grid style.
+ * The template for displaying archive pages.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package nelumbo
  */
-
+ 
 get_header(); ?>
 
-<div id="primary" class="content-area grid-container archive-grid-template">
-	<main id="main" class="site-main grid-x grid-padding-x" data-equalizer data-equalize-by-row="true" role="main">
-		<?php if ( have_posts() ) : ?>
-			<header class="page-header cell">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header.cell -->
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-			?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class('large-4 medium-6 cell'); ?>>
-				<a class="card" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" data-equalizer-watch>
+<div class="grid-container">
+<?php if ( ! is_active_sidebar( 'sidebar-1' )  ) : ?>
+	<div class="grid-x grid-padding-x align-center">
+		<div id="primary" class="content-area large-8 medium-10 align-center">
+<?php else : ?>
+	<div class="grid-x grid-padding-x">
+		<div id="primary" class="content-area large-7 large-collapse-right medium-8 cell">
+<?php endif; ?>
+			<main id="main" class="site-main" role="main">
+				<?php if ( have_posts() ) : ?>
+					<header class="page-header">
+						<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+						?>
+					</header><!-- .page-header -->
 					<?php
-					if ( has_post_thumbnail() ) : ?>
-						<?php the_post_thumbnail('entry-featured-img'); ?>
-					<?php endif; ?>
-					<div class="card-section">
-						<?php the_title( '<h2 class="h4 entry-title">', '</h2>' ); ?>
-						<div class="entry-excerpt">
-							<?php the_excerpt(); ?>
-						</div>
-					</div>
-				</a>
-			</article><!-- #post-##.large-4.medium-6 -->
-			<?php
-			endwhile;
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-		endif;
-		?>
-	</main><!-- #main.grid-x -->
-</div><!-- #primary.grid-container -->
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+						/**
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_format() );
+					endwhile;
+				else :
+					get_template_part( 'template-parts/content', 'none' );
+				endif;
+				?>
+			</main><!-- #main -->
+		</div><!-- #primary.large-7 -->
+	<?php get_sidebar(); ?>
+	</div><!-- .grid-x -->
+</div><!-- .grid-container -->
 <?php
 if ( get_the_posts_navigation() ) :
 	get_template_part( 'template-parts/post-navigation' );
