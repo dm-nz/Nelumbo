3294ad6c819bf3b80ac592542ef044c5b653725a
dmnz <nazzzim@gmail.com>
2018-12-24 14:18:47 +0600

    Style comment navigation

--- comments.php
+++ comments.php
@@ -33,15 +33,6 @@ if ( have_comments() ) : ?>
 		);
 		?>
 	</h2><!-- .comments-title -->
-	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-	<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-		<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'nelumbo' ); ?></h2>
-		<div class="nav-links">
-			<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'nelumbo' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'nelumbo' ) ); ?></div>
-		</div><!-- .nav-links -->
-	</nav><!-- #comment-nav-above -->
-	<?php endif; // Check for comment navigation. ?>
 	<ol class="comment-list">
 		<?php
 		wp_list_comments( array(
